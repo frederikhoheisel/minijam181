0909b586310370ac9d5e580d6a8c6ca128908d22
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2025-03-30 17:03:04 +0200

    added stats as global

---
+++ minijam181/globals/stats.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var current_horde_size: int = 0
+var total_deaths: int = 0
+var mine_deaths: int = 0
+var hunter_deaths: int = 0
+var zap_deaths: int = 0
+var drown_deaths: int = 0
+var bird_deaths: int = 0
+
+func _ready() -> void:
+	SignalBus.rabbit_died.connect(_on_rabbit_died)
+	
+
+func _on_rabbit_died(rabbit: RigidBody2D, death_type: String, pos: Vector2) -> void:
+	total_deaths +=1
+	match death_type:
+		"headshot": 
+			hunter_deaths += 1
+		"landmine":
+			mine_deaths += 1
+		"rain":
+			zap_deaths += 1
+		"river":
+			drown_deaths += 1
+		"default": 
+			return
